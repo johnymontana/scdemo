68f7393c4c61826d476a2e41c20c8899bf670ae1
johnymontana <lyonwj@gmail.com>
2016-08-27 18:05:00 -0300

    Update import script
    Use py2neo v3
    Add categories in single cypher statement

--- yelp_import.py
+++ yelp_import.py
@@ -1,19 +1,24 @@
 import json
-from py2neo.neo4j import CypherQuery, GraphDatabaseService, WriteBatch
-from py2neo import neo4j
+from py2neo import Graph
 
-db = neo4j.GraphDatabaseService()
+db = Graph('http://45.55.182.50:7474/')
 
-business_index_query = CypherQuery(db, "CREATE INDEX ON :Business(id)")
-business_index_query.execute()
+db.run("CREATE INDEX ON :Business(id)")
+db.run("CREATE INDEX ON :Category(name)")
 
-category_index_query = CypherQuery(db, "CREATE INDEX ON :Category(name)")
-category_index_query.execute()
 
 create_business_query = '''
 // MERGE ON categories
-CREATE (b:Business {id: {business_id}, name: {name}, lat:{latitude}, lon:{longitude},
-	stars: {stars}, review_count: {review_count}})
+MERGE (b:Business {id: {business_id}})
+ON CREATE SET b.name = {name}, 
+	b.latitude = {latitude}, 
+	b.longitude = {longitude},
+	b.stars = {stars}, 
+	b.review_count = {review_count}
+WITH b
+UNWIND {categories} AS category
+MERGE (c:Category {name: category})
+MERGE (b)-[:IS_IN]->(c)
 '''
 
 merge_category_query = '''
@@ -22,31 +27,37 @@ MERGE (c:Category {name: {category}})
 CREATE UNIQUE (c)<-[:IS_IN]-(b)
 '''
 
-print "Beginning business batch"
+
+
+print("Beginning business batch")
 with open('data/yelp_academic_dataset_business.json', 'r') as f:
-	business_batch = WriteBatch(db)
+	tx = db.begin()
 	count = 0
 	for b in (json.loads(l) for l in f):
-		business_batch.append_cypher(create_business_query, b)
+		tx.run(create_business_query, b)
 		count += 1
 		if count >= 10000:
-			business_batch.run()
-			business_batch.clear()
+			tx.commit()
+			tx = db.begin()
+			print("Committing transaction")
 			count = 0
 	if count > 0:
-		business_batch.run()
+		tx.commit()
+		print("Committing transaction")
 
-print "Beginning category batch"
-with open('data/yelp_academic_dataset_business.json', 'r') as f:
-	category_batch = WriteBatch(db)
-	count = 0
-	for b in (json.loads(l) for l in f):
-		for c in b['categories']:
-			category_batch.append_cypher(merge_category_query, {'business_id': b['business_id'], 'category': c})
-			count += 1
-			if count >= 10000:
-				category_batch.run()
-				category_batch.clear()
-				count = 0
-	if count > 0:
-		category_batch.run()
+# print ("Beginning category batch")
+# with open('data/yelp_academic_dataset_business.json', 'r') as f:
+# 	tx = db.begin()
+# 	count = 0
+# 	for b in (json.loads(l) for l in f):
+# 		for c in b['categories']:
+# 			tx.run(merge_category_query, {'business_id': b['business_id'], 'category': c})
+# 			count += 1
+# 			if count >= 10000:
+# 				tx.commit()
+# 				print("Committing transaction")
+# 				tx = db.begin()
+# 				count = 0
+# 	if count > 0:
+# 		tx.commit()
+# 		print("Committing transaction")
